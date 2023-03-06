55078368f6f6f15eaf898416f5a1b79ba84781b7
Diana P <diana.pavulane@gmail.com>
2023-03-07 00:32:44 +0200

    Updated withdraw.py

---
+++ withdraw.py
@@ -0,0 +1,11 @@
+current_balance = 100
+withdraw_amount = int(input("Enter the amount to withdraw: "))
+def withdraw(withdraw_amt):
+    if withdraw_amt > current_balance:
+        print("Can't withdraw", withdraw_amt, "because you don't have enough money")
+        print("Try with lower amount than balance")
+    else:
+        print(f"You have withdrawn {withdraw_amount} from your account,you remaining balance is", (current_balance-withdraw_amount))
+
+
+withdraw(withdraw_amount)
