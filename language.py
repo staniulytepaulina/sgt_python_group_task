e5b7a99b86ba131e415dae882a6f26ac70acd289
Paulina Staniulyte <staniulyte.paulina@gmail.com>
2023-03-07 19:59:28 +0200

    minor improvement

--- language.py
+++ language.py
@@ -7,5 +7,5 @@ def choose_language():
         print(f"You've selected {language.capitalize()} language.")
         return language.capitalize()
     else:
-        print(f"We didn't understand you. We will keep English language for you.")
+        print("We didn't understand you. We will keep English language for you.")
         return "English"
