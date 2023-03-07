e3b4e82ede0017159208b7c6274a70033c3bb9ca
Paulina Staniulyte <staniulyte.paulina@gmail.com>
2023-03-07 20:11:40 +0200

    minor improvement

--- withdraw.py
+++ withdraw.py
@@ -6,6 +6,7 @@ def withdraw():
     if withdraw_amt > constants.BALANCE:
         print("Can't withdraw", withdraw_amt, "because you don't have enough money")
         print("Try with lower amount than balance")
+        withdraw()
     else:
         print(f"You have withdrawn {withdraw_amt} from your account, your remaining balance is", (constants.BALANCE - withdraw_amt))
         return constants.BALANCE - withdraw_amt
